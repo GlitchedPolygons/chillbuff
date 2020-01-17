3ff5ec7c84b13305f066b6b87d767f125038fde4
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-01-17 15:19:40 +0100

    Added some basic tests for the init function.

--- tests/chillbuff_tests.c
+++ tests/chillbuff_tests.c
@@ -18,19 +18,47 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include "../include/chillbuff.h"
 
 /* A test case that does nothing and succeeds. */
-static void null_test_success(void **state) 
-{    
-	(void)state;
+static void null_test_success(void** state)
+{
+    (void)state;
+}
+
+static void chillbuff_init_buff_arg_null_returns_CHILLBUFF_NULL_ARG(void** state)
+{
+    assert_int_equal(CHILLBUFF_NULL_ARG, chillbuff_init(NULL, 8, 1, CHILLBUFF_GROW_DUPLICATIVE));
+}
+
+static void chillbuff_element_size_0_returns_CHILLBUFF_INVALID_ARG(void** state)
+{
+    chillbuff b;
+    assert_int_equal(CHILLBUFF_INVALID_ARG, chillbuff_init(&b, 16, 0, CHILLBUFF_GROW_DUPLICATIVE));
+}
+
+static void chillbuff_element_invalid_grow_method_returns_CHILLBUFF_INVALID_ARG(void** state)
+{
+    chillbuff b;
+    assert_int_equal(CHILLBUFF_INVALID_ARG, chillbuff_init(&b, 16, 1, -1));
+    assert_int_equal(CHILLBUFF_INVALID_ARG, chillbuff_init(&b, 16, 1, 200));
+}
+
+static void chillbuff_init_returns_CHILLBUFF_SUCCESS(void** state)
+{
+    chillbuff b;
+    assert_int_equal(CHILLBUFF_SUCCESS, chillbuff_init(&b, 16, 1, CHILLBUFF_GROW_DUPLICATIVE));
 }
 
 int main(void)
 {
-	const struct CMUnitTest tests[] = 
-	{        
-		cmocka_unit_test(null_test_success),    
-	};
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+        cmocka_unit_test(chillbuff_init_buff_arg_null_returns_CHILLBUFF_NULL_ARG),
+        cmocka_unit_test(chillbuff_element_size_0_returns_CHILLBUFF_INVALID_ARG),
+        cmocka_unit_test(chillbuff_element_invalid_grow_method_returns_CHILLBUFF_INVALID_ARG),
+        cmocka_unit_test(chillbuff_init_returns_CHILLBUFF_SUCCESS),
+    };
 
-	return cmocka_run_group_tests(tests, NULL, NULL);
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
